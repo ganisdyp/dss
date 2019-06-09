3f71250acdb5fe65f66726bb07366e641c5729bb
unknown <clbs@T1LP0008.berlin-hq.local>
2019-06-09 23:05:39 +0700

    db config & url pretty config

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=binapile_dss',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
